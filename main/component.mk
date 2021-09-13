1c85882393160858a5af3294dd408f020014ab7a
Mauricio Barroso Benavides <mauriciobarroso1990@gmail.com>
2021-09-13 09:40:20 -0400

    Updated according the current certificates

--- main/component.mk
+++ main/component.mk
@@ -3,4 +3,4 @@
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
-COMPONENT_EMBED_TXTFILES := ../certs/AmazonRootCA1.pem ../certs/device.pem.crt ../certs/private.pem.key
+COMPONENT_EMBED_TXTFILES := ../certs/server.pem
