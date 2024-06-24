ac0a0d4babf575b9a6e440b682612bec93c64e85
Mauricio Barroso Benavides <mauriciobarroso1990@gmail.com>
2024-06-24 11:10:00 -0400

    updated script to select esp device

--- prod_flash.sh
+++ prod_flash.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Get OTA URL and server certificate paths
-OTA_FILE_URL=$1
-OTA_SERVER_CERT=$2
+ESP_TARGET=$1
+OTA_FILE_URL=$2
+OTA_SERVER_CERT=$3
 
 # Create sdkconfig.temp to store OTA parameters
 touch sdkconfig.temp
@@ -19,8 +20,8 @@ echo "CONFIG_OTA_ENABLE=y" >> $SDKCONFIG_TEMP
 echo "CONFIG_OTA_FILE_URL=\"$OTA_FILE_URL\"" >> $SDKCONFIG_TEMP
 echo "CONFIG_OTA_SERVER_CERT=\"$OTA_SERVER_CERT\"" >> $SDKCONFIG_TEMP
 
-# Build project with security options for ESP32-S3
-idf.py -D SDKCONFIG_DEFAULTS="sdkconfig.defaults;sdkconfig.security;sdkconfig.temp" set-target esp32s3 build
+# Build project with security options
+idf.py -D SDKCONFIG_DEFAULTS="sdkconfig.defaults;sdkconfig.security;sdkconfig.temp" set-target $ESP_TARGET build
 
 # Flash bootloader, app, ota data and NVS data
-esptool.py --chip esp32s3 -p /dev/ttyUSB1 -b 460800 --before=default_reset --after=no_reset --no-stub write_flash --flash_mode dio --flash_freq 80m --flash_size keep 0x0 build/bootloader/bootloader.bin 0x20000 build/NearFi.bin 0x8000 build/partition_table/partition-table.bin 0x11000 build/ota_data_initial.bin
+#esptool.py --chip $ESP_TARGET -b 460800 --before=default_reset --after=no_reset --no-stub write_flash --flash_mode dio --flash_freq 80m --flash_size keep 0x0 build/bootloader/bootloader.bin 0x20000 build/NearFi.bin 0x8000 build/partition_table/partition-table.bin 0x11000 build/ota_data_initial.bin
